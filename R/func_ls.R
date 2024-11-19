e95de5379b38e2ce5a80a316f26f7cdd920668a9
robertomolinari <roberto.molinari@hotmail.it>
2024-11-19 10:13:47 -0600

    commit of first draft of package

---
+++ R/func_ls.R
@@ -0,0 +1,36 @@
+loss_ls <- function(X, y, beta) {
+
+  # Add a column of 1s to X to include the intercept in the model
+  X <- cbind(rep(1, n), X)
+
+  # Calculate the sum of squared residuals between observed values (y) and predicted values (X %*% beta)
+  sum((y - X %*% beta)^2)
+
+}
+
+
+#' @title LS through optimization
+#'
+#' @description XXX
+#' @param y A \code{vector} of response.
+#' @param X A \code{matrix} of predictors.
+#' @return A \code{list} containing the following attributes:
+#' \describe{XX}
+#' @author Roberto Molinari
+#' @importFrom stats runif
+#' @export
+#' @examples
+#' n <- 100
+#' set.seed(123)
+#' X <- cbind(rnorm(n), rpois(n, lambda = 3))
+#' beta <- c(2, -3, 1) # true beta
+#' design <- cbind(rep(1, n), X)
+#' y <- design %*% beta + rnorm(n)
+#' opt_ls(y = y, X = X)
+opt_ls <- function(y, X) {
+
+  p <- ncol(X) + 1
+
+  optim(rep(0, p), fn = loss_ls, X = X, y = y)$par
+
+}
